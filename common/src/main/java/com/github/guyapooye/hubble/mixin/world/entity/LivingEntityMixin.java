f0da047eea7e1184319d95075901c249b124a099
James <jgreenop22@gmail.com>
2025-06-09 21:23:34 -0700

    disable void damage in space

---
+++ common/src/main/java/com/github/guyapooye/hubble/mixin/world/entity/LivingEntityMixin.java
@@ -0,0 +1,30 @@
+package com.github.guyapooye.hubble.mixin.world.entity;
+
+import com.github.guyapooye.hubble.Hubble;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+
+    public LivingEntityMixin(EntityType<?> type, Level level) {
+        super(type, level);
+    }
+
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    private void hurtMixin(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (source == this.damageSources().fellOutOfWorld()) {
+            if (this.level().dimension().location().equals(Hubble.SPACE)) {
+                // Cancel void damage
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
